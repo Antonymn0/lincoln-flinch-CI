f8142e01e54ca74730cab9ea172d9573b946fc7c
antonymn0 <mnantony0@gmail.com>
2021-11-01 00:51:33 +0300

    faker seeder update

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -8,16 +8,17 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
+        $faker = \Faker\Factory::create();
         for ($i = 0; $i < 10; $i++) { 
             $model = model('UserModel');
             $model->insert([
-                'name'=> static::faker()->firstName,
-                'email'=> static::faker()->email,
-                'phone'=> static::faker()->phoneNumber,
-                'file_type'=> static::faker()->randomElement(['jpj', 'png']),
+                'name'=> $faker->name,
+                'email'=> $faker->email,
+                'phone'=> $faker->phoneNumber,
+                'file_type'=> $faker->randomElement(['jpj', 'png']),
                 'file'=> 'https://images.unsplash.com/photo-1491484925566-336b202157a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
-                'status'=> static::faker()->randomElement(['complete', 'pending']),
-                'password'=> static::faker()->password,
+                'status'=> $faker->randomElement(['complete', 'pending']),
+                'password'=> $faker->password,
                 'deleted_at'=> null,
             ]);
         }
